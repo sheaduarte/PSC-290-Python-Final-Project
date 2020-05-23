1169758e639cac962f8dcf255c89a17cf33727eb
sheaduarte <33766275+sheaduarte@users.noreply.github.com>
2020-05-23 15:17:01 -0700

    worked on MSD datacleaning notebook

--- AnalysisHelper.py
+++ AnalysisHelper.py
@@ -68,9 +68,7 @@ def remove_outliers(df, var, outlier_constant = 1.5):
 	return(cleanTrials)		
 	
 def EyeTracking_MasterDF(df, currentFixationIdx, nearestIAVar, currentFixDurationVar, fixationTotalVar, trialVar = 'trial', participantVar = 'participant'):
-    '''This function takes a raw eye tracking dataframe and condenses it down to 
-    the number of trials in the task by creating a summary row for each trial'''
-    EyeDF_cols = []
+    EyeDF_cols = ['trial','participant', 'total_fixations', 'latency', 'fixation0']
     EyeTracking_MasterDF = pd.DataFrame(columns = EyeDF_cols)
     Fix0 = df[df[currentFixationIdx] == 1]
     EyeTracking_MasterDF['trial'] = Fix0[trialVar]
@@ -83,11 +81,11 @@ def EyeTracking_MasterDF(df, currentFixationIdx, nearestIAVar, currentFixDuratio
     for x in list(range(2,7)):
         FixDFs = df[df[currentFixationIdx] == x]
         FixDFs = FixDFs[keep_cols]
-        FixDFs = FixDFs.rename(columns = {nearestIAVar:'fixation' + str(x), currentFixDurationVar:'fix_dur' + str(x)})
+        FixDFs = FixDFs.rename(columns = {nearestIAVar:'fixation' + str(x-1), currentFixDurationVar:'fix_dur_' + str(x-1)})
         dfs.append(FixDFs)
     EyeTracking_MasterDF = reduce(lambda left,right: pd.merge(left,right,on=['trial', 'participant'], how='outer'), dfs)
     return EyeTracking_MasterDF
-	
+		
 def First_Fixation(df, fixation1Var, fixation2Var, fixation3Var, fixationIA = 'fixationIA'):
 	'''Used to disregard first fixations that are on the fixation cross (the fixation interest area (IA)) 
 	so that first fixations refer to the first non-fixation interest area that participants look at'''
@@ -110,118 +108,118 @@ def Add_FirstFixDwell(df, fixation1Var, fixation2Var, fixDuration1, fixDuration2
 ########## Creating Figures ##########
 
 def histogram(df, y):
-    '''Requires altair and altair saver. Plots a histogram of y and automatically saves a html file 
-    to output directory. Won't work if df has more than 5000 rows.'''
-    if df.shape[0] >5000:
-        return 'The dataframe is too large. Subset the data or use a different dataframe.'
-    else:
-        chart = alt.Chart(df).mark_bar().encode(alt.X(y,title= ylab[0], bin = True), y = 'count()', )
-        chart.save(output_directory+'Histogram of '+y+'.html')
-        return chart
+	'''Requires altair and altair saver. Plots a histogram of y and automatically saves a html file 
+	to output directory. Won't work if df has more than 5000 rows.'''
+	if df.shape[0] >5000:
+		return 'The dataframe is too large. Subset the data or use a different dataframe.'
+	else:
+		chart = alt.Chart(df).mark_bar().encode(alt.X(y,title= ylab[0], bin = True), y = 'count()', )
+		chart.save(output_directory+'Histogram of '+y+'.html')
+		return chart
 
 def bar_graph(df,x,y,z):
-    '''Requires seaborn. Plots a bar graph of x by y, grouped by z if desired. Automatically saves 
-    a png file to output directory. '''
-    g = sns.catplot(x=x, y=y, 
-                hue=z, # use this to group, if needed
-                data=df,
-                height=6, kind="bar")
-    g.despine(left=True)
-    g.set_ylabels(ylab[0])
-    g.set_xlabels(xlab[0])
-    #g.set_xticklabels(rotation=45) #can turn off if you don't need axes rotated
-    g.set_titles('Mean Differences in '+ylab[0])
-    g.savefig(output_directory+ 'Mean Differences in '+y+'.png')
-    return g
+	'''Requires seaborn. Plots a bar graph of x by y, grouped by z if desired. Automatically saves 
+	a png file to output directory. '''
+	g = sns.catplot(x=x, y=y, 
+				hue=z, # use this to group, if needed
+				data=df,
+				height=6, kind="bar")
+	g.despine(left=True)
+	g.set_ylabels(ylab[0])
+	g.set_xlabels(xlab[0])
+	#g.set_xticklabels(rotation=45) #can turn off if you don't need axes rotated
+	g.set_titles('Mean Differences in '+ylab[0])
+	g.savefig(output_directory+ 'Mean Differences in '+y+'.png')
+	return g
 
-def stacked_bar_graph(df,x,y,z):
-    '''Requires altair and altair saver. Plots a standardized stacked bar graph of x by y, where z 
-    is different subgroups within X. Automatically saves a html file to output directory. '''
-    chart = alt.Chart(df).mark_bar().encode(
-        x=x,
-        y='sum('+y+')',
-        color=alt.Color(z, 
-        scale=alt.Scale(scheme='dark2')) #changes color scheme. 
-        # see https://vega.github.io/vega/docs/schemes/ for examples
-    ).properties(
-    title='Proportions of 'z+' by '+x)
-    chart.save(output_directory+'Proportions of 'z+' by '+x+'.html')
-    return chart
+# def stacked_bar_graph(df,x,y,z):
+#	  '''Requires altair and altair saver. Plots a standardized stacked bar graph of x by y, where z
+#	  is different subgroups within X. Automatically saves a html file to output directory. '''
+#	  chart = alt.Chart(df).mark_bar().encode(
+#		  x=x,
+#		  y='sum('+y+')',
+#		  color=alt.Color(z,
+#		  scale=alt.Scale(scheme='dark2')) #changes color scheme.
+#		  # see https://vega.github.io/vega/docs/schemes/ for examples
+#	  ).properties(
+#	  title='Proportions of 'z+' by '+x)
+#	chart.save(output_directory+'Proportions of 'z+' by '+x+'.html')
+#	  return chart
 
 def scatter_plot(df,x,y,z):
-    '''Requires altair and altair saver. Plots a scatter plot of x and y, where z 
-    is a factor that changes point color (optional). Tooltip functionality enabled, but will 
-    need to specify desired columns ahead of time. Automatically saves a html file to output directory. '''
-    chart= alt.Chart(df).mark_circle(size=60).encode(
-    x=x,
-    y=y,
-    color=alt.Color(z, 
-        scale=alt.Scale(scheme='pastel1')),
-    tooltip= tt_interactive
-    ).interactive().properties(
-    title='Scatterplot of '+x+' by '+y)
-    chart.save(output_directory+'Scatterplot of '+x+' by '+y+'.html')
-    return chart
+	'''Requires altair and altair saver. Plots a scatter plot of x and y, where z 
+	is a factor that changes point color (optional). Tooltip functionality enabled, but will 
+	need to specify desired columns ahead of time. Automatically saves a html file to output directory. '''
+	chart= alt.Chart(df).mark_circle(size=60).encode(
+	x=x,
+	y=y,
+	color=alt.Color(z, 
+		scale=alt.Scale(scheme='pastel1')),
+	tooltip= tt_interactive
+	).interactive().properties(
+	title='Scatterplot of '+x+' by '+y)
+	chart.save(output_directory+'Scatterplot of '+x+' by '+y+'.html')
+	return chart
 
 def scatter_matrix(df,x,z):
-    '''Requires altair and altair saver. Plots a scatter matrix of a list of variables (x), where z 
-    is a factor that changes point color (optional). Automatically saves a html file to output directory. '''
-    x_inverse = x[::-1] 
-    chart= alt.Chart(df).mark_circle().encode(
-        alt.X(alt.repeat("column"), type='quantitative'),
-        alt.Y(alt.repeat("row"), type='quantitative'),
-        color=alt.Color(z+':N', 
-        scale=alt.Scale(scheme='accent'))
-    ).properties(
-        width=150,
-        height=150
-    ).repeat(
-        row=x,
-        column= x_inverse
-    ).interactive()
-    chart.save(output_directory+'Scatterplot Matrix.html')
-    return chart
+	'''Requires altair and altair saver. Plots a scatter matrix of a list of variables (x), where z 
+	is a factor that changes point color (optional). Automatically saves a html file to output directory. '''
+	x_inverse = x[::-1] 
+	chart= alt.Chart(df).mark_circle().encode(
+		alt.X(alt.repeat("column"), type='quantitative'),
+		alt.Y(alt.repeat("row"), type='quantitative'),
+		color=alt.Color(z+':N', 
+		scale=alt.Scale(scheme='accent'))
+	).properties(
+		width=150,
+		height=150
+	).repeat(
+		row=x,
+		column= x_inverse
+	).interactive()
+	chart.save(output_directory+'Scatterplot Matrix.html')
+	return chart
 
 def violin(df,x,y,z):
-    '''Requires seaborn. Plots a violin distribution plot of y by x  where z 
-    is a factor that allows for grouping, if desired. DOES NOT AUTOMATICALLY SAVE OUTPUT. '''
-    ax = sns.violinplot(x=x, y=y, 
-                    hue=z, #optional
-                    data=df)
-    ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-    return ax
+	'''Requires seaborn. Plots a violin distribution plot of y by x	 where z 
+	is a factor that allows for grouping, if desired. DOES NOT AUTOMATICALLY SAVE OUTPUT. '''
+	ax = sns.violinplot(x=x, y=y, 
+					hue=z, #optional
+					data=df)
+	ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+	return ax
 
 def regression_plot(df,x,y,z):
-    '''Requires seaborn. Plots a regression plot of x by y with regression lines, where z 
-    is a factor that allows for grouping, if desired. Automatically saves output as a png file. '''
-    g = sns.lmplot(x=x, y=y, hue=z,
-               data=df)
-    g.savefig(output_directory+ 'Regression Plot of '+x+' and '+y+'.png')
+	'''Requires seaborn. Plots a regression plot of x by y with regression lines, where z 
+	is a factor that allows for grouping, if desired. Automatically saves output as a png file. '''
+	g = sns.lmplot(x=x, y=y, hue=z,
+			   data=df)
+	g.savefig(output_directory+ 'Regression Plot of '+x+' and '+y+'.png')
 
 def boxplot(df,x,y,z):
-    '''Requires seaborn. Plots a boxplot of y by x with marks for outliers,, where z 
-    is a factor that allows for grouping, if desired. DOES NOT AUTOMATICALLY SAVE OUTPUT. '''
-    ax = sns.boxplot(x=x, y=y,
-                hue=z,
-                data=df)
-    sns.despine(offset=10, trim=True)
-    ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+	'''Requires seaborn. Plots a boxplot of y by x with marks for outliers,, where z 
+	is a factor that allows for grouping, if desired. DOES NOT AUTOMATICALLY SAVE OUTPUT. '''
+	ax = sns.boxplot(x=x, y=y,
+				hue=z,
+				data=df)
+	sns.despine(offset=10, trim=True)
+	ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
 
 ##### Data Subsetting for Eyetracking Data Figures #####
 
 def FirstFixProportions(df, condition_list, IA_1='targetIA', IA_2='distIA', trialVar = 'trial'):
-    '''Uses dataframe of count values & finds proportion of first fixations to two interest areas, 
-    not suitable for more than two interest areas'''
-    result = pd.DataFrame()
-    for condition in condition_list:
-        target = (df[(trialVar, condition)][IA_1])/((df[(trialVar,condition)][IA_2])+
-                                                          (df[(trialVar, condition)][IA_1]))
-        distractor = (df[(trialVar, condition)][IA_2])/((df[(trialVar, condition)][IA_2])+
-                                                            (df[(trialVar, condition)][IA_1]))
-        Proportion_Dict = {'condition':condition,'target': target,'distractor':distractor}
-        result = result.append(Proportion_Dict, ignore_index =True)
-    return result
+	'''Uses dataframe of count values & finds proportion of first fixations to two interest areas, 
+	not suitable for more than two interest areas'''
+	result = pd.DataFrame()
+	for condition in condition_list:
+		target = (df[(trialVar, condition)][IA_1])/((df[(trialVar,condition)][IA_2])+
+														  (df[(trialVar, condition)][IA_1]))
+		distractor = (df[(trialVar, condition)][IA_2])/((df[(trialVar, condition)][IA_2])+
+															(df[(trialVar, condition)][IA_1]))
+		Proportion_Dict = {'condition':condition,'target': target,'distractor':distractor}
+		result = result.append(Proportion_Dict, ignore_index =True)
+	return result
 
 
 
